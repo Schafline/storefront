afd1c3c8fdf33fbaf8e5d36706f8836f23cc4545
DESKTOP-B62L5TF\jimcr <jim.craig43@hotmail.com>
2025-11-04 16:57:40 -0300

    Clean up bin and obj folders

--- Pages/Basket.cshtml.cs
+++ Pages/Basket.cshtml.cs
@@ -1,13 +1,40 @@
+
+using Microsoft.AspNetCore.Mvc;
+
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using storefront.Models;
 
 public class BasketModel : PageModel
 {
-    public List<BasketItem> Items { get; set; } = new();
+    public List<Product> BasketItems { get; set; } = new();
+    [BindProperty]
+    public int Id { get; set; }
+
+    public decimal TotalPrice { get; set; }
+
+    public IActionResult OnPostRemove()
+    {
+        var basketJson = TempData["Basket"] as string ?? "[]";
+        var basketItems = System.Text.Json.JsonSerializer.Deserialize<List<Product>>(basketJson) ?? new List<Product>();
+
+        var itemToRemove = basketItems.FirstOrDefault(p => p.Id == Id);
+        if (itemToRemove != null)
+        {
+            basketItems.Remove(itemToRemove);
+            TempData["Basket"] = System.Text.Json.JsonSerializer.Serialize(basketItems);
+        }
+
+        return RedirectToPage();
+    }
+
 
     public void OnGet()
     {
-        Items.Add(new BasketItem { Name = "T-shirt", Quantity = 2 });
-        Items.Add(new BasketItem { Name = "Sneakers", Quantity = 1 });
+        var basketJson = TempData["Basket"] as string ?? "[]";
+        BasketItems = System.Text.Json.JsonSerializer.Deserialize<List<Product>>(basketJson)?? new List<Product>();
+        TotalPrice = BasketItems.Sum(p => p.Price);
+        // Keep basket alive for future requests
+        TempData.Keep("Basket");
+
     }
 }
