0f721daa5bde59825ec0c9f2e895d3cb0da9d345
James Craig Haddad <200680845+Schafline@users.noreply.github.com>
2026-05-14 23:04:53 +0100

    Chapter 2: Building the Shopping Basket

---
+++ Models/BasketItem.cs
@@ -0,0 +1,7 @@
+namespace Storefront.Models;
+
+public class BasketItem
+{
+  public string Name { get; set; }
+  public int Quantity { get; set; }
+}
---
+++ Pages/Basket.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Storefront.Models;
+
+namespace Storefront.Pages;
+
+public class BasketModel : PageModel
+{
+  public List<BasketItem> Items { get; set; } = new();
+
+  public void OnGet()
+  {
+    Items.Add(new BasketItem { Name = "T-shirt", Quantity = 2 });
+    Items.Add(new BasketItem { Name = "Sneakers", Quantity = 1 });
+  }
+}
